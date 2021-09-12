76e0d9b3947bf4d17c41a5d5948a6e7e1e978469
philiplorenz-code <84645793+philiplorenz-code@users.noreply.github.com>
2021-09-13 01:00:41 +0200

    ewfg

--- code/dummy/post.php
+++ code/dummy/post.php
@@ -1,4 +1,32 @@
 <?php 
-    echo $_POST['name']; 
-    echo $_POST['email']; 
+
+?>
+
+
+<?php
+$name = $_POST['name']; 
+$email = $_POST['email']; 
+
+$servername = "localhost";
+$username = "webd_philip";
+$password = "nvzHol-#p6PEu#ai";
+$dbname = "webd_db";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+// Check connection
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+} 
+
+$sql = "INSERT INTO test (name, email)
+VALUES ($name, $email)";
+
+if ($conn->query($sql) === TRUE) {
+  echo "New record created successfully";
+} else {
+  echo "Error: " . $sql . "<br>" . $conn->error;
+}
+
+$conn->close();
 ?>
