861ced010dcdbadede80fa9ad66aa4aa0d1f2229
philiplorenz-code <84645793+philiplorenz-code@users.noreply.github.com>
2021-09-13 01:07:31 +0200

    dfgh

--- code/dummy/post.php
+++ code/dummy/post.php
@@ -1,8 +1,3 @@
-<?php 
-
-?>
-
-
 <?php
 $name = $_POST['name']; 
 $email = $_POST['email']; 
@@ -27,6 +22,6 @@ if ($conn->query($sql) === TRUE) {
 } else {
   echo "Error: " . $sql . "<br>" . $conn->error;
 }
-
+$myfile = fopen("new.html", "w")
 $conn->close();
 ?>
