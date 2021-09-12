dff2fda7ebb05ffafe40931576355fd3be978b07
philiplorenz-code <84645793+philiplorenz-code@users.noreply.github.com>
2021-09-13 01:18:29 +0200

    dsc

--- code/dummy/post.php
+++ code/dummy/post.php
@@ -1,7 +1,25 @@
 <?php
-$name = $_POST['name']; 
-$email = $_POST['email']; 
 
+function generateRandomString($length = 10) {
+    $characters = 'abcdefghijklmnopqrstuvwxyz';
+    $charactersLength = strlen($characters);
+    $randomString = '';
+    for ($i = 0; $i < $length; $i++) {
+        $randomString .= $characters[rand(0, $charactersLength - 1)];
+    }
+    return $randomString;
+}
+
+// Vars for database
+$vorname = $_POST['vorname']; 
+$name = $_POST['name']; 
+$beschreibung = $_POST['beschreibung']; 
+$beruf = $_POST['beruf']; 
+$wunschgehalt = $_POST['wunschgehalt']; 
+$email = $_POST['email']; 
+$pageid = generateRandomString();
+
+// Database Config
 $servername = "localhost";
 $username = "webd_philip";
 $password = "nvzHol-#p6PEu#ai";
@@ -14,8 +32,8 @@ if ($conn->connect_error) {
   die("Connection failed: " . $conn->connect_error);
 } 
 
-$sql = "INSERT INTO test (name, email)
-VALUES ('$name', '$email')";
+$sql = "INSERT INTO userprofile (Vorname, Name, Beschreibung, Beruf, Wunschgehalt, Email, Pageid)
+VALUES ('$vorname', '$name', '$beschreibung', '$beruf', '$wunschgehalt', '$email', '$pageid')";
 
 if ($conn->query($sql) === TRUE) {
   echo "New record created successfully";
