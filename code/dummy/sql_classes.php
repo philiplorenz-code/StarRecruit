3e72641031e9e062785739ebe14caab82f11f22c
philiplorenz-code <84645793+philiplorenz-code@users.noreply.github.com>
2021-09-14 02:16:59 +0200

    dsf

---
+++ code/dummy/sql_classes.php
@@ -0,0 +1,34 @@
+<?php 
+
+// SQL Server Config
+$sqlConfig = array(
+    "servername" => "localhost",
+    "username" => "webd_philip",
+    "password" => "nvzHol-#p6PEu#ai",
+    "dbname" => "users"
+);
+
+function getAllUsers($sqlConf) {
+    // Create connection
+    $conn = new mysqli($sqlConfig["servername"], $sqlConfig["username"], $sqlConfig["password"], $sqlConfig["dbname"]);
+
+    // Check connection
+    if ($conn->connect_error) {
+        die("Connection failed: " . $conn->connect_error);
+    } 
+  
+    // SQL Insert
+    $sql = "SELECT * FROM 'users'";
+    $result = $conn->query($sql);
+    if ($result === TRUE) {
+        echo $result;
+    } else {
+        echo "Error: " . $sql . "<br>" . $conn->error;
+    }
+
+    // Close Connection
+    $conn->close();
+
+}
+
+getAllUsers($sqlConfig);
