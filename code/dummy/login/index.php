5e824a3319cc079030145b8ddeadd9884746152d
philiplorenz-code <84645793+philiplorenz-code@users.noreply.github.com>
2021-09-15 22:33:08 +0200

    dsf

--- code/dummy/login/index.php
+++ code/dummy/login/index.php
@@ -1,15 +1,23 @@
+<?php
+
+
+
+?>
+
 <html>
 <body>
+    <a href="logout.php">Logout</a>
+    <h1>INDEX</h1>
 
-<form action="post.php" method="post">
-Vorname: <input type="text" name="vorname"><br>
-Name: <input type="text" name="name"><br>
-Beschreibung: <input type="text" name="beschreibung"><br>
-Beruf: <input type="text" name="beruf"><br>
-Wunschgehalt: <input type="text" name="wunschgehalt"><br>
-E-mail: <input type="text" name="email"><br>
-<input type="submit">
-</form>
+    <form action="post.php" method="post">
+    Vorname: <input type="text" name="vorname"><br>
+    Name: <input type="text" name="name"><br>
+    Beschreibung: <input type="text" name="beschreibung"><br>
+    Beruf: <input type="text" name="beruf"><br>
+    Wunschgehalt: <input type="text" name="wunschgehalt"><br>
+    E-mail: <input type="text" name="email"><br>
+    <input type="submit">
+    </form>
 
 </body>
 </html>
