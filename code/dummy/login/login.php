315cbc9821ee707c25235182272ea89c7dd42d94
philiplorenz-code <84645793+philiplorenz-code@users.noreply.github.com>
2021-09-15 23:11:04 +0200

    sde

--- code/dummy/login/login.php
+++ code/dummy/login/login.php
@@ -50,8 +50,8 @@ session_start();
 		<form method="post">
 			<div style="font-size: 20px;margin: 10px;color: white;">Login</div>
 
-			<input id="text" type="text" name="user_name"><br><br>
-			<input id="text" type="password" name="password"><br><br>
+			<input id="text" type="text" name="user_email"><br><br>
+			<input id="text" type="password" name="user_password"><br><br>
 
 			<input id="button" type="submit" value="Login"><br><br>
 
