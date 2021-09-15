ba55c11582cff8b06ebcd3e545c8a24825b6f421
philiplorenz-code <84645793+philiplorenz-code@users.noreply.github.com>
2021-09-15 23:38:02 +0200

    dfg

--- code/dummy/login/login.php
+++ code/dummy/login/login.php
@@ -3,6 +3,34 @@ session_start();
 
     include("connection.php");
     include("functions.php");
+
+    if($_SERVER['REQUEST_METHOD'] == "POST"){
+        //sth was posted
+        $user_email = $_POST['user_email'];
+        $user_password = $_POST['user_password'];
+
+        if(!empty($user_email) && !empty($user_password) && !is_numeric($user_email)){
+            // read to db
+            $query = "select * from users where user_email = $'$user_email' limit 1;";
+            $result = mysqli_query($con, $query);
+
+            if($result){
+                if($result && mysqli_num_rows($result) > 0){
+                    $user_data = mysqli_fetch_assoc($result);
+                    if($user_data['user_password'] === $user_password){
+                        $_SESSION['user_id'] = $user_data['user_id'];
+                        header("Location: index.php");
+                        die;
+                    }
+                }
+            }
+
+
+        }
+        else {
+            echo "Please enter valid information!!!";
+    }
+}
     
 
 ?>
