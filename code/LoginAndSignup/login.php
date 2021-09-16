152f21b3e5f2ea52fd9b33a209cd517035190f97
Philip <philip_lorenz@icloud.com>
2021-09-16 10:51:33 +0200

    dsf

--- code/LoginAndSignup/login.php
+++ code/LoginAndSignup/login.php
@@ -58,7 +58,9 @@ session_start();
                     <h1 style="text-align: center;color: #9c3f2a;"><i class="fa fa-lightbulb-o"></i>&nbsp;StarRecruitment</h1>
                     <form method="post">
                         <div class="form-group mb-3"><label class="form-label text-secondary">Email</label><input class="form-control" type="text" name="user_email" required="" pattern="[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,15}$" inputmode="email"></div>
-                        <div class="form-group mb-3"><label class="form-label text-secondary">Password</label><input class="form-control" type="password" name="user_password" required=""></div><button class="btn btn-info mt-2" type="submit" style="background: #9c3f2a;">Log In</button>
+                        <div class="form-group mb-3"><label class="form-label text-secondary">Password</label><input class="form-control" type="password" name="user_password" required=""></div>
+                        <div class="form-group mb-3"><label class="form-label text-secondary">Password</label><input class="form-control" type="password" name="user_password" required=""></div>
+                        <button class="btn btn-info mt-2" type="submit" style="background: #9c3f2a;">Log In</button>
                     </form><a href="signup.php" style="text-align: right;">Not having an account? Click here to Sign-Up!</a>
                     <p class="mt-3 mb-0"></p>
                 </div>
