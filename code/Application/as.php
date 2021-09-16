2cb1cdb3b2d3ae5682dcc7fb7f90b8e260dba831
Philip <philip_lorenz@icloud.com>
2021-09-16 23:06:47 +0200

    df

--- code/Application/as.php
+++ code/Application/as.php
@@ -200,25 +200,20 @@ session_start();
     </div>
     <table>
             <tbody>
-            <tr>
-            <th>  ffffffffffffff      </th>
-            </tr>
-            <tr>
-            <tr>
             <th><form action="upload.php" method="post" enctype="multipart/form-data">
-            Lade Zeugnisse und Zertifikate hoch (in einer Datei):
+                      Lade Zeugnisse und Zertifikate hoch (in einer Datei):
             <input type="file" name="fileToUpload" id="fileToUpload">
             <input type="submit" value="Upload Image" name="submit"></form></th>
             </tr>
             <tr>
             <th><form action="upload.php" method="post" enctype="multipart/form-data">
-            Lade Zeugnisse und Zertifikate hoch (in einer Datei):
+                      Lade Zeugnisse und Zertifikate hoch (in einer Datei):
             <input type="file" name="fileToUpload" id="fileToUpload">
             <input type="submit" value="Upload Image" name="submit"></form></th>
             </tr>
             <tr>
             <th><form action="upload.php" method="post" enctype="multipart/form-data">
-            Lade Zeugnisse und Zertifikate hoch (in einer Datei):
+                       Lade Zeugnisse und Zertifikate hoch (in einer Datei):
             <input type="file" name="fileToUpload" id="fileToUpload">
             <input type="submit" value="Upload Image" name="submit"></form></th>
             </tr>
