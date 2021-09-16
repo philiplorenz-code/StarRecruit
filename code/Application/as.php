970f61c377f47a9331079dbcb8eba64cb954a67f
Philip <philip_lorenz@icloud.com>
2021-09-16 22:33:42 +0200

    df

--- code/Application/as.php
+++ code/Application/as.php
@@ -200,6 +200,8 @@ session_start();
                         </form>
                     </div>
                 </div>
+            </div>
+            <div>
                 <div class="col-xl-6 col-lg-6 col-md-6 col-sm-6 col-12">
                     <div class="form-group">
                         <label for="beruf">Lebenslauf</label>
