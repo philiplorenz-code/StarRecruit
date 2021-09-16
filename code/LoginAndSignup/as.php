13d1994d2db3d314a62305d88bd0d24d5a2176aa
Philip <philip_lorenz@icloud.com>
2021-09-16 11:47:45 +0200

    dsf

--- code/LoginAndSignup/as.php
+++ code/LoginAndSignup/as.php
@@ -15,15 +15,15 @@ session_start();
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
     <title>Dashboard - Brand</title>
-    <link rel="stylesheet" href="assets/bootstrap/css/bootstrap.min.css">
+    <link rel="stylesheet" href="assets_as/bootstrap/css/bootstrap.min.css">
     <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i">
-    <link rel="stylesheet" href="assets/fonts/fontawesome-all.min.css">
-    <link rel="stylesheet" href="assets/fonts/font-awesome.min.css">
-    <link rel="stylesheet" href="assets/fonts/fontawesome5-overrides.min.css">
-    <link rel="stylesheet" href="assets/css/Account-setting-or-edit-profile.css">
-    <link rel="stylesheet" href="assets/css/FORM.css">
-    <link rel="stylesheet" href="assets/css/iframe.css">
-    <link rel="stylesheet" href="assets/css/untitled.css">
+    <link rel="stylesheet" href="assets_as/fonts/fontawesome-all.min.css">
+    <link rel="stylesheet" href="assets_as/fonts/font-awesome.min.css">
+    <link rel="stylesheet" href="assets_as/fonts/fontawesome5-overrides.min.css">
+    <link rel="stylesheet" href="assets_as/css/Account-setting-or-edit-profile.css">
+    <link rel="stylesheet" href="assets_as/css/FORM.css">
+    <link rel="stylesheet" href="assets_as/css/iframe.css">
+    <link rel="stylesheet" href="assets_as/css/untitled.css">
 </head>
 
 <body id="page-top">
