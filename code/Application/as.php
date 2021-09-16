23202b609899e1d75c5568c56fe7913bf34f9a9b
Philip <philip_lorenz@icloud.com>
2021-09-16 23:13:19 +0200

    sf

--- code/Application/as.php
+++ code/Application/as.php
@@ -198,7 +198,7 @@ session_start();
         </div>
     </div>
     </div>
-    <div class="tableupload">
+    <div style="text-align:center;">
             <table>
             <tbody>
             <th><form action="upload.php" method="post" enctype="multipart/form-data">
