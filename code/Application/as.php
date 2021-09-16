df96b9c33d926a60df7f21e45a81ca1991912146
Philip <philip_lorenz@icloud.com>
2021-09-16 22:49:13 +0200

    s

--- code/Application/as.php
+++ code/Application/as.php
@@ -190,6 +190,26 @@ session_start();
 
 
 
+            <table>
+            <tbody>
+            <tr>
+            <th>Table Header first column</th>
+            <th>Table Header second column</th>
+            <th>Table Header third column</th>
+            </tr>
+            <tr>
+            <td>data goes here</td>
+            <td>data goes here</td>
+            <td>data goes here</td>
+            </tr>
+            <tr>
+            <td>more data here</td>
+            <td>more data here</td>
+            <td>more data here</td>
+            </tr>
+            </tbody>
+            </table>
+
 
 
             <form action="upload.php" method="post" enctype="multipart/form-data">
