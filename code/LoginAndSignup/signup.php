bfeba56782463f68a5ec1cc7d6a3f81a8020b332
Philip <philip_lorenz@icloud.com>
2021-09-16 10:28:33 +0200

    dsv

--- code/LoginAndSignup/signup.php
+++ code/LoginAndSignup/signup.php
@@ -1,3 +1,29 @@
+<?php
+session_start();
+
+    include("connection.php");
+    include("functions.php");
+
+    if($_SERVER['REQUEST_METHOD'] == "POST"){
+        //sth was posted
+        $user_email = $_POST['user_email'];
+        $user_password = $_POST['user_password'];
+
+        if(!empty($user_email) && !empty($user_password) && !is_numeric($user_email)){
+            // save to db
+            $query = "insert into users (user_email,user_password) values ('$user_email','$user_password');";
+            mysqli_query($con, $query);
+
+            header("Location: login.php");
+            die;
+        }
+        else {
+            echo "Please enter valid information!!!";
+        }
+    }
+    
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 
