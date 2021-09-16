8c070415ce0dc2af691403f464fa802fd38f5f4e
Philip <philip_lorenz@icloud.com>
2021-09-16 20:55:05 +0200

    sfd

--- code/Application/as.php
+++ code/Application/as.php
@@ -135,7 +135,7 @@ session_start();
             </div>
             <div class="row gutters">
                 <div class="col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12">
-                    <h6 class="mt-3 mb-2 text-primary">Fähigkeiten (getrennt durch +)</h6>
+                    <h6 class="mt-3 mb-2 text-primary">Fähigkeiten (getrennt durch Komma (,))</h6>
                 </div>
                 <div class="col-xl-6 col-lg-6 col-md-6 col-sm-6 col-12">
                     <div class="form-group">
