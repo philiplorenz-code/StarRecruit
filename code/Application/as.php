1d4f9248a7ba5f0a94f5dec63b50e229b2fb2691
Philip <philip_lorenz@icloud.com>
2021-09-16 20:12:12 +0200

    f

--- code/Application/as.php
+++ code/Application/as.php
@@ -22,6 +22,8 @@ session_start();
 
         // save to db
         $query = "update users set vorname='$vorname',name='$name',wohnort='$wohnort',user_alter='$user_alter',beschreibung='$beschreibung',sprachen='$sprachen',softskills='$softskills',hardskills='$hardskills',mind_gehalt='$mind_gehalt',beruf='$beruf' where user_id='$user_id';";
+        echo $beschreibung;
+        echo $_POST['beschreibung'];
         echo $query;
         mysqli_query($con, $query);
 
