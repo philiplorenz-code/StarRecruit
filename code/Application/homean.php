cc7136773c8c267864daff2f582fe59449feaba6
philiplorenz-code <84645793+philiplorenz-code@users.noreply.github.com>
2021-09-23 02:42:39 +0200

    fdh

--- code/Application/homean.php
+++ code/Application/homean.php
@@ -63,7 +63,7 @@ session_start();
         <nav class="navbar navbar-dark align-items-start sidebar sidebar-dark accordion bg-gradient-primary p-0" style="color: var(--bs-purple);background: #9c3f2a;">
             <div class="container-fluid d-flex flex-column p-0"><a class="navbar-brand d-flex justify-content-center align-items-center sidebar-brand m-0" href="#">
                     <div class="sidebar-brand-icon rotate-n-15"><i class="far fa-lightbulb"></i></div>
-                    <div class="sidebar-brand-text mx-3"><span>StarRecruitment</span></div>
+                    <div class="sidebar-brand-text mx-3"><span>SR</span></div>
                 </a>
                 <hr class="sidebar-divider my-0">
                 <ul class="navbar-nav text-light" id="accordionSidebar">
