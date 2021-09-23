99c11fc8e65b74c7d0748c900d52a90dd288c774
philiplorenz-code <84645793+philiplorenz-code@users.noreply.github.com>
2021-09-23 06:01:33 +0200

    dfg

--- code/Application/searchag.php
+++ code/Application/searchag.php
@@ -68,32 +68,31 @@ session_start();
             <div class="row register-form">
                 <div class="col-md-8 offset-md-2">
                     <form class="custom-form">
-                        <h1>Register Form</h1>
+                        <h1>Neue Suche</h1>
                         <div class="row form-group">
-                            <div class="col-sm-4 label-column"><label class="col-form-label" for="name-input-field">Name </label></div>
+                            <div class="col-sm-4 label-column"><label class="col-form-label" for="name-input-field">Stadt </label></div>
                             <div class="col-sm-6 input-column"><input class="form-control" type="text"></div>
                         </div>
                         <div class="row form-group">
-                            <div class="col-sm-4 label-column"><label class="col-form-label" for="email-input-field">Email </label></div>
+                            <div class="col-sm-4 label-column"><label class="col-form-label" for="email-input-field">HardSkills </label></div>
                             <div class="col-sm-6 input-column"><input class="form-control" type="email"></div>
                         </div>
                         <div class="row form-group">
-                            <div class="col-sm-4 label-column"><label class="col-form-label" for="pawssword-input-field">Password </label></div>
+                            <div class="col-sm-4 label-column"><label class="col-form-label" for="pawssword-input-field">Softskills </label></div>
                             <div class="col-sm-6 input-column"><input class="form-control" type="password"></div>
                         </div>
                         <div class="row form-group">
-                            <div class="col-sm-4 label-column"><label class="col-form-label" for="repeat-pawssword-input-field">Repeat Password </label></div>
+                            <div class="col-sm-4 label-column"><label class="col-form-label" for="repeat-pawssword-input-field">Gehalt (max) </label></div>
                             <div class="col-sm-6 input-column"><input class="form-control" type="password"></div>
                         </div>
                         <div class="row form-group">
-                            <div class="col-sm-4 label-column"><label class="col-form-label" for="dropdown-input-field">Dropdown </label></div>
+                            <div class="col-sm-4 label-column"><label class="col-form-label" for="dropdown-input-field">Wochenstunden </label></div>
                             <div class="col-sm-4 input-column">
                                 <div class="dropdown"><button class="btn btn-light dropdown-toggle" aria-expanded="false" data-bs-toggle="dropdown" type="button">Dropdown </button>
                                     <div class="dropdown-menu"><a class="dropdown-item" href="#">First Item</a><a class="dropdown-item" href="#">Second Item</a><a class="dropdown-item" href="#">Third Item</a></div>
                                 </div>
                             </div>
                         </div>
-                        <div class="form-check"><input class="form-check-input" type="checkbox" id="formCheck-1"><label class="form-check-label" for="formCheck-1">I've read and accept the terms and conditions</label></div><button class="btn btn-light submit-button" type="button">Submit Form</button>
                     </form>
                 </div>
             </div>
