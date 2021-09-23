b6e3dc33564fda7aff7f532bbcf0b0f655a4ecd3
philiplorenz-code <84645793+philiplorenz-code@users.noreply.github.com>
2021-09-23 05:50:02 +0200

    df

--- code/Application/homeag.php
+++ code/Application/homeag.php
@@ -7,7 +7,7 @@ session_start();
     $user_data = check_login($con);
     $user_id = $user_data["user_id"];
 
-    // Get all messages to user
+    // Get username
     $query = "select vorname from users where user_id='$user_id';";
     $result = mysqli_query($con, $query);
     $entry = $result->fetch_assoc();
@@ -29,16 +29,15 @@ session_start();
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
     <title>Dashboard - Brand</title>
-    <link rel="stylesheet" href="assets/bootstrap/css/bootstrap.min.css">
+    <link rel="stylesheet" href="assets_as/bootstrap/css/bootstrap.min.css">
     <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i">
-    <link rel="stylesheet" href="assets/fonts/fontawesome-all.min.css">
-    <link rel="stylesheet" href="assets/fonts/font-awesome.min.css">
-    <link rel="stylesheet" href="assets/fonts/fontawesome5-overrides.min.css">
-    <link rel="stylesheet" href="assets/css/Animated-Typing-With-Blinking.css">
-    <link rel="stylesheet" href="assets/css/FPE-Gentella-form-elements-1.css">
-    <link rel="stylesheet" href="assets/css/FPE-Gentella-form-elements.css">
-    <link rel="stylesheet" href="assets/css/Multi-step-form.css">
-    <link rel="stylesheet" href="assets/css/Pretty-Registration-Form.css">
+    <link rel="stylesheet" href="assets_as/fonts/fontawesome-all.min.css">
+    <link rel="stylesheet" href="assets_as/fonts/font-awesome.min.css">
+    <link rel="stylesheet" href="assets_as/fonts/fontawesome5-overrides.min.css">
+    <link rel="stylesheet" href="assets_as/css/Account-setting-or-edit-profile.css">
+    <link rel="stylesheet" href="assets_as/css/FORM.css">
+    <link rel="stylesheet" href="assets_as/css/iframe.css">
+    <link rel="stylesheet" href="assets_as/css/untitled.css">
 </head>
 
 <body id="page-top">
@@ -50,27 +49,31 @@ session_start();
                 </a>
                 <hr class="sidebar-divider my-0">
                 <ul class="navbar-nav text-light" id="accordionSidebar">
-                    <li class="nav-item"><a class="nav-link" href="https://webdev.learning-it.io/Application/homeag.php"><i class="fas fa-table"></i><span>Home</span></a></li>
+                <li class="nav-item"><a class="nav-link" href="https://webdev.learning-it.io/Application/homeag.php"><i class="fas fa-table"></i><span>Home</span></a></li>
                     <li class="nav-item"><a class="nav-link" href="https://webdev.learning-it.io/Application/sucheag.php"><i class="fas fa-table"></i><span>Suchen bearbeiten</span></a></li>
                     <li class="nav-item"><a class="nav-link" href="https://webdev.learning-it.io/Application/editprofileag.php"><i class="fas fa-table"></i><span>Profil Bearbeiten</span></a></li>
                     <li class="nav-item"><a class="nav-link" href="https://webdev.learning-it.io/Application/postfachag.php"><i class="fas fa-table"></i><span>Postfach</span></a></li>
                     <li class="nav-item"><a class="nav-link" href="https://webdev.learning-it.io/Application/postfachag.php"><i class="fas fa-table"></i><span>Einstellungen</span></a></li>
                     <li class="nav-item"><a class="nav-link" href="https://webdev.learning-it.io/Application/postfachag.php"><i class="fas fa-table"></i><span>Logout</span></a></li>
-
-
                     <li class="nav-item"></li>
                     <li class="nav-item"></li>
                 </ul>
             </div>
         </nav>
-
         <div class="d-flex flex-column" id="content-wrapper">
             <div class="p-5 mb-4 bg-light round-3">
                 <div class="container-fluid py-5">
                     <h1 class="display-5 fw-bold">Hallo <?php echo $entry['vorname']; ?>!</h1>
+                    <p class="col-md-8 fs-4">"Meaning is something you build into your life. You build it out of your own past, out of your affections and loyalties, out of the experience of humankind as it is passed on to you... You are the only one who can put them together into that unique pattern that will be your life."</p>
                     <h1>Du hast <?php echo $num_rows; ?> neue Nachrichten!</h1><a class="btn btn-primary btn-lg" role="button" href="https://webdev.learning-it.io/Application/postfachan.php">Zum Postfach</a>
                 </div>
-        </div>
+            </div>
+            <div class="p-5 mb-4 bg-light round-3">
+                <div class="container-fluid py-5">
+                    <h1 class="display-5 fw-bold">News-Feed</h1>
+                    <h1>HIER DER FEED</h1>
+                </div>
+            </div>
         </div>
         </div>
 
