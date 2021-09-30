838a16557fc89eafce67d192e157c5f23cf34d4c
philiplorenz-code <84645793+philiplorenz-code@users.noreply.github.com>
2021-09-30 22:05:09 +0200

    sd

--- code/Application/matching_algo.php
+++ code/Application/matching_algo.php
@@ -2,6 +2,7 @@
 include("connection.php");
 include("functions.php");
 
+$ReqPoints = 3;
 
 // GET ALL USERS
 $Users = [];
@@ -12,6 +13,15 @@ while ($row = $result->fetch_assoc()) {
 }
 //print_r($Users);
 
+
+// GET ALL MATCHES
+$DBMatches = [];
+$query = "select * from users where user_acctype='Arbeitssuchender';";
+$result = mysqli_query($con, $query);
+while ($row = $result->fetch_assoc()) {
+    array_push($DBMatches,$row);
+}
+//print_r($Users);
 
 
 // GET ALL SEARCHES
@@ -24,18 +34,47 @@ while ($row = $result->fetch_assoc()) {
 //print_r($Searches);
 
 
-// MATCHES RAW
+// Set PreMatch Values
 $PreMatches = [];
+foreach ($Searches as $search){
+    foreach ($Users as $user){
+        $PreMatch = [];
+        $PreMatch["SearchID"] = $search["id"];
+        $PreMatch["UserID"] = $user["user_id"];
 
+        array_push($PreMatches, $PreMatch);
+    }
+}
+
+print_r($PreMatches)
+
+/*
+
+// MATCHES RAW - Just for orientation
+//$PreMatches = [];
 // CFG
-$PreMatch = [
-    "Gehalt" => "0";
-    "PLZ" => "0";
-    "Hardskills" => "0";
-    "Softskills" => "0";
-    "Sprachen" => "0";
-]
+$PreMatch34 = array(
+    "SearchID" => 1,
+    "UserID" => 1,
+    "Gehalt" => 0,
+    "PLZ" => 0,
+    "Hardskills" => 0,
+    "Softskills" => 0,
+    "Sprachen" => 0,
+);
+array_push($PreMatches34,$PreMatch34);
 
-array_push($PreMatches,$PreMatch);
 
-echo "0" + "0";
+// Matches (Fitting SUM and not existing already)
+$Matches = [];
+
+// IF SUM >= 3
+// AND
+// IF (! DBSearch==Search && DBUser == User)
+// ----> $PreMatch -> $Match
+
+
+// All Matches To DB And Transform to Message (Status open from searchid->recruiterid to userid)
+
+
+
