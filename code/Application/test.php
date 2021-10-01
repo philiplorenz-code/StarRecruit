2c466a501872643269d89c22d02e818ab2189e16
philiplorenz-code <84645793+philiplorenz-code@users.noreply.github.com>
2021-10-01 12:46:06 +0200

    gh

--- code/Application/test.php
+++ code/Application/test.php
@@ -3,5 +3,6 @@
 $query_getunternehmenid = "select recruiter_id from search where id='1';";
 $resultunternehmensid = mysqli_query($con, $query_getunternehmenid);
 $row = $resultunternehmensid->fetch_assoc();
+echo $row;
 $rec_id = $row["recruiter_id"];
 echo $rec_id;
