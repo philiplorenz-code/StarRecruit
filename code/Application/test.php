2f635c4ad997acb713ecb370eea937fb4af06c3b
philiplorenz-code <84645793+philiplorenz-code@users.noreply.github.com>
2021-10-01 12:46:37 +0200

    df

--- code/Application/test.php
+++ code/Application/test.php
@@ -1,6 +1,7 @@
 <?php
 // Create new Message
 $query_getunternehmenid = "select recruiter_id from search where id='1';";
+echo $query_getunternehmenid;
 $resultunternehmensid = mysqli_query($con, $query_getunternehmenid);
 $row = $resultunternehmensid->fetch_assoc();
 echo $row;
