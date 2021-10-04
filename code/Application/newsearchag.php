8702e82a15e70a184121f9c369b173651796d70e
philiplorenz-code <84645793+philiplorenz-code@users.noreply.github.com>
2021-10-04 20:15:56 +0200

    fg

--- code/Application/newsearchag.php
+++ code/Application/newsearchag.php
@@ -85,6 +85,7 @@
                                     <label for="vorname">Name der Suche</label>
                                     <input type="text" class="form-control" name="name">
                                  </div>
+         </br>
                               </div>
                            <div class="row gutters">
                               <div class="col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12">
