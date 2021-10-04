42795fd2f42b39500ce29dc32dc15c795baf758e
philiplorenz-code <84645793+philiplorenz-code@users.noreply.github.com>
2021-10-04 19:29:22 +0200

    removed query echo

--- code/Application/newsearchag.php
+++ code/Application/newsearchag.php
@@ -25,7 +25,6 @@
                $query = "INSERT INTO search (stadt, softskills, hardskills, sprachen, max_gehalt, wochenstunden,recruiter_id) VALUES ('$stadt', '$softskills', '$hardskills', '$sprachen', '$gehalt', '$wochenstunden', '$recruiterid');";
 
             
-               echo $query;
                // echo $query;
                mysqli_query($con, $query);
        
