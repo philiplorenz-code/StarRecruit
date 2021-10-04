da082939a400aafabab5483748f88c39eb36e2ad
philiplorenz-code <84645793+philiplorenz-code@users.noreply.github.com>
2021-10-04 21:02:02 +0200

    df

--- code/Application/test.php
+++ code/Application/test.php
@@ -1,9 +1,5 @@
 <?php
-include("connection.php");
-include("functions.php");
-// Create new Message
-$query_getunternehmenid = "select recruiter_id from search where id='1';";
-$resultunternehmensid = mysqli_query($con, $query_getunternehmenid);
-$row = $resultunternehmensid->fetch_assoc();
-$rec_id = $row["recruiter_id"];
-echo $rec_id;
+$jsonurl = "https://quotes.rest/qod";
+$json = file_get_contents($jsonurl);
+var_dump(json_decode($json));
+
