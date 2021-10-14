fcfaec5da4a7f373e16653e56003021026f41fa0
Philip <philip_lorenz@icloud.com>
2021-10-14 10:52:46 +0200

    sd

--- code/Arbeitgeber/home.php
+++ code/Arbeitgeber/home.php
@@ -1,8 +1,8 @@
 <?php
    session_start();
    
-       include("connection.php");
-       include("functions.php");
+       include("code/ApplicationLogic/connection.php");
+       include("fcode/ApplicationLogic/functions.php");
 
        
        $user_data = check_login($con);
