3d26eeda2ad6de78037b19b6a24b613e1186a249
Philip <philip_lorenz@icloud.com>
2021-10-14 21:22:40 +0200

    sd

--- code/Arbeitnehmer/postfachan.php
+++ code/Arbeitnehmer/postfachan.php
@@ -1,3 +1,47 @@
+<?php
+session_start();
+
+    include("connection.php");
+    include("functions.php");
+    
+    $user_data = check_login($con);
+    $user_id = $user_data["user_id"];
+
+    // Get all messages to user
+    $query = "select * from nachrichten where user_id='$user_id'"; // and status='accepted';
+    $result = mysqli_query($con, $query);
+
+    if($_SERVER['REQUEST_METHOD'] == "POST"){
+
+        if(isset($_POST['invite'])){
+            $nachricht_id = $_POST['invite'];
+            $query = "UPDATE nachrichten SET status='eingeladengespraech' WHERE id='$nachricht_id';";
+            mysqli_query($con, $query);
+            header("Location: postfachag.php");
+            die;
+        }
+        elseif(isset($_POST['assess'])){
+            $nachricht_id = $_POST['assess'];
+            $query = "UPDATE nachrichten SET status='eingeladenAssess' WHERE id='$nachricht_id';";
+            mysqli_query($con, $query);
+            header("Location: postfachag.php");
+            die;
+        }
+        elseif(isset($_POST['del'])){
+          $nachricht_id = $_POST['del'];
+          $query = "DELETE FROM nachrichten WHERE id='$nachricht_id';";
+          mysqli_query($con, $query);
+          header("Location: postfachag.php");
+          die;
+      }
+        else{
+        }
+    }
+
+
+
+?>
+
 <!DOCTYPE html>
 <html lang="en" dir="ltr">
   <head>
@@ -46,85 +90,6 @@
           </a>
         </li>
       </ul>
-  </div>    <div class="logo-details">
-      <i class='bx bxs-bulb bx-md'></i>
-      <span class="logo_name">StarRecruitment</span>
-    </div>
-      <ul class="nav-links">
-        <li>
-          <a href="./home.html">
-            <i class='bx bx-grid-alt' ></i>
-            <span class="links_name">Account-Start</span>
-          </a>
-        </li>
-        <li>
-          <a href="./editsearch.html">
-            <i class='bx bx-box' ></i>
-            <span class="links_name">Profil Bearbeiten</span>
-          </a>
-        </li>
-        <li>
-          <a href="./profile.html">
-            <i class='bx bx-list-ul' ></i>
-            <span class="links_name">Posteingang</span>
-          </a>
-        </li>
-        <li>
-          <a href="./settings.html">
-            <i class='bx bx-coin-stack' ></i>
-            <span class="links_name">Einstellungen</span>
-          </a>
-        </li>
-        <li class="log_out">
-          <a href="#">
-            <i class='bx bx-log-out'></i>
-            <span class="links_name">Log out</span>
-          </a>
-        </li>
-      </ul>
-  </div>
-    <div class="logo-details">
-      <i class='bx bxl-c-plus-plus'></i>
-      <span class="logo_name">StarRecruitment</span>
-    </div>
-      <ul class="nav-links">
-        <li>
-          <a href="./home.html">
-            <i class='bx bx-grid-alt' ></i>
-            <span class="links_name">Home</span>
-          </a>
-        </li>
-        <li>
-          <a href="./editsearch.html">
-            <i class='bx bx-box' ></i>
-            <span class="links_name">Suchen Bearbeiten</span>
-          </a>
-        </li>
-        <li>
-          <a href="./profile.html">
-            <i class='bx bx-list-ul' ></i>
-            <span class="links_name">Profil Bearbeiten</span>
-          </a>
-        </li>
-        <li>
-          <a href="./postfachag.html" class="active">
-            <i class='bx bx-pie-chart-alt-2' ></i>
-            <span class="links_name">Postfach</span>
-          </a>
-        </li>
-        <li>
-          <a href="./settings.html">
-            <i class='bx bx-coin-stack' ></i>
-            <span class="links_name">Einstellungen</span>
-          </a>
-        </li>
-        <li class="log_out">
-          <a href="#">
-            <i class='bx bx-log-out'></i>
-            <span class="links_name">Log out</span>
-          </a>
-        </li>
-      </ul>
   </div>
   <section class="home-section">
     <nav>
@@ -148,49 +113,22 @@
                      <th>Status</th>
                      <th>Bewerbungsgespräch</th>
                      <th>Assessmentcenter</th>
+                     <th>Löschen</th>
                   </tr>
                </thead>
-               <tr>
-                  <td><a href="#">INV1001</a></td>
-                  <td>Paragon</td>
-                  <td>1/5/2021</td>
-                  <td>
-                     <p class="status status-unpaid">Unpaid</p>
-                  </td>
-               </tr>
-               <tr>
-                  <td><a href="#">INV1002</a></td>
-                  <td>Sonic</td>
-                  <td>1/4/2021</td>
-                  <td>
-                     <p class="status status-paid">Paid</p>
-                  </td>
-               </tr>
-               <tr>
-                  <td><a href="#">INV1003</a></td>
-                  <td>Innercircle</td>
-                  <td>1/2/2021</td>
-                  <td>
-                     <p class="status status-pending">Pending</p>
-                  </td>
-               </tr>
-               <tr>
-                  <td><a href="#">INV1004</a></td>
-                  <td>Varsity Plus</td>
-                  <td>12/30/2020</td>
-                  <td>
-                     <p class="status status-pending">Pending</p>
-                  </td>
-               </tr>
-               <tr>
-                  <td><a href="#">INV1005</a></td>
-                  <td>Highlander</td>
-                  <td>12/18/2020</td>
-                  <td>
-                     <p class="status status-paid">Paid</p>
-                  </td>
-               </tr>
-               </tr>
+
+               <?php
+                while ($row = $result->fetch_assoc()) {
+                        echo "<tr>";
+                        echo "<td>" . $row['id'] . "</td>";
+                        echo "<td>" . $row['status'] . "</td>";
+                        echo "<td> " . " <form method='post'> <button type='submit' name='invite' value=" . $row['id'] . "> Einladung zu Gespräch </button> </form>" . "</td>";
+                        echo "<td> " . " <form method='post'> <button type='submit' name='assess' value=" . $row['id'] . "> Einladung zu AssessmentCenter </button> </form>" . "</td>";
+                        echo "<td> " . " <form method='post'> <button type='submit' name='del' value=" . $row['id'] . "> Löschen </button> </form>" . "</td>";
+                        echo "</tr>";
+                }
+              ?>
+
             </table>
         </div>
 
