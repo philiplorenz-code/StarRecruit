2471a61315366385ab1a05724254eb17247eef62
Philip <philip_lorenz@icloud.com>
2021-10-14 21:21:44 +0200

    sd

--- code/Arbeitnehmer/profile.php
+++ code/Arbeitnehmer/profile.php
@@ -1,3 +1,45 @@
+<?php
+   session_start();
+   
+       include("connection.php");
+       include("functions.php");
+       
+       $user_data = check_login($con);
+       $user_id = $user_data["user_id"];
+   
+   
+   
+           // Post der Form
+           if($_SERVER['REQUEST_METHOD'] == "POST"){
+            //sth was posted
+            $user_id = $user_data["user_id"];
+            $unternehmensname = $_POST['unternehmensname'];
+            $mitarbeiteranzahl = $_POST['mitarbeiteranzahl'];
+            $branche = $_POST['branche'];
+            $website = $_POST['website'];
+            $verantwortlicher = $_POST['verantwortlicher'];
+    
+            // save to db
+            $query = "update users set unternehmensname='$unternehmensname',mitarbeiteranzahl='$mitarbeiteranzahl',branche='$branche',website='$website',verantwortlicher='$verantwortlicher' where user_id='$user_id';";
+
+            // echo $query;
+            mysqli_query($con, $query);
+
+
+            // exec("php matching_algo.php");
+
+    
+        }
+   
+           $query = "select * from users where user_id='$user_id';";
+
+           $result = mysqli_query($con, $query);
+           $entry = $result->fetch_assoc();
+           //var_dump($entry);
+           //echo $entry['user_email'];
+
+?>
+
 <!DOCTYPE html>
 <html lang="en" dir="ltr">
   <head>
@@ -46,85 +88,6 @@
           </a>
         </li>
       </ul>
-  </div>    <div class="logo-details">
-      <i class='bx bxs-bulb bx-md'></i>
-      <span class="logo_name">StarRecruitment</span>
-    </div>
-      <ul class="nav-links">
-        <li>
-          <a href="./home.html">
-            <i class='bx bx-grid-alt' ></i>
-            <span class="links_name">Account-Start</span>
-          </a>
-        </li>
-        <li>
-          <a href="./editsearch.html">
-            <i class='bx bx-box' ></i>
-            <span class="links_name">Profil Bearbeiten</span>
-          </a>
-        </li>
-        <li>
-          <a href="./profile.html">
-            <i class='bx bx-list-ul' ></i>
-            <span class="links_name">Posteingang</span>
-          </a>
-        </li>
-        <li>
-          <a href="./settings.html">
-            <i class='bx bx-coin-stack' ></i>
-            <span class="links_name">Einstellungen</span>
-          </a>
-        </li>
-        <li class="log_out">
-          <a href="#">
-            <i class='bx bx-log-out'></i>
-            <span class="links_name">Log out</span>
-          </a>
-        </li>
-      </ul>
-  </div>
-    <div class="logo-details">
-      <i class='bx bxl-c-plus-plus'></i>
-      <span class="logo_name">StarRecruitment</span>
-    </div>
-      <ul class="nav-links">
-        <li>
-          <a href="./home.html">
-            <i class='bx bx-grid-alt' ></i>
-            <span class="links_name">Home</span>
-          </a>
-        </li>
-        <li>
-          <a href="./editsearch.html">
-            <i class='bx bx-box' ></i>
-            <span class="links_name">Suchen Bearbeiten</span>
-          </a>
-        </li>
-        <li>
-          <a href="./profile.html">
-            <i class='bx bx-list-ul' ></i>
-            <span class="links_name">Profil Bearbeiten</span>
-          </a>
-        </li>
-        <li>
-          <a href="./postfachag.html" class="active">
-            <i class='bx bx-pie-chart-alt-2' ></i>
-            <span class="links_name">Postfach</span>
-          </a>
-        </li>
-        <li>
-          <a href="./settings.html">
-            <i class='bx bx-coin-stack' ></i>
-            <span class="links_name">Einstellungen</span>
-          </a>
-        </li>
-        <li class="log_out">
-          <a href="#">
-            <i class='bx bx-log-out'></i>
-            <span class="links_name">Log out</span>
-          </a>
-        </li>
-      </ul>
   </div>
   <section class="home-section">
     <nav>
@@ -144,40 +107,40 @@
 
             <div id="fcf-form">
         
-            <form id="fcf-form-id" class="fcf-form-class" method="post" action="contact-form-process.php">
+            <form id="fcf-form-id" class="fcf-form-class" method="post">
                 
                 <div class="fcf-form-group">
                     <label for="Name" class="fcf-label">Unternehmensname</label>
                     <div class="fcf-input-group">
-                        <input type="text" id="Name" name="Name" class="fcf-form-control" required>
+                        <input type="text" id="Name" class="fcf-form-control" name="unternehmensname" required value='<?php echo $entry['unternehmensname'];?>'>
                     </div>
                 </div>
         
                 <div class="fcf-form-group">
-                    <label for="Email" class="fcf-label">Mitarbeiteranzahl</label>
+                    <label for="Name" class="fcf-label">Mitarbeiteranzahl</label>
                     <div class="fcf-input-group">
-                        <input type="email" id="Email" name="Email" class="fcf-form-control" required>
+                        <input type="text" id="Name" class="fcf-form-control" name="mitarbeiteranzahl" required value='<?php echo $entry['mitarbeiteranzahl'];?>'>
                     </div>
                 </div>
         
                 <div class="fcf-form-group">
                     <label for="Message" class="fcf-label">Branche</label>
                     <div class="fcf-input-group">
-                        <input id="text" name="Message" class="fcf-form-control" rows="6" required></input>
+                        <input id="text" class="fcf-form-control" rows="6" name="branche" required value='<?php echo $entry['branche'];?>'></input>
                     </div>
                 </div>
 
                 <div class="fcf-form-group">
                   <label for="Message" class="fcf-label">Link zur Website</label>
                   <div class="fcf-input-group">
-                      <input id="text" name="Message" class="fcf-form-control" rows="6" required></input>
+                      <input id="text" class="fcf-form-control" rows="6" name="website" required value='<?php echo $entry['website'];?>'></input>
                   </div>
               </div>
 
               <div class="fcf-form-group">
                 <label for="Message" class="fcf-label">Verantwortlicher</label>
                 <div class="fcf-input-group">
-                    <input id="text" name="Message" class="fcf-form-control" rows="6" required></input>
+                    <input id="text" class="fcf-form-control" rows="6" name="verantwortlicher" required value='<?php echo $entry['verantwortlicher'];?>'></input>
                 </div>
             </div>
         
