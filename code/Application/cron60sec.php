bb331f3a0d670eb7d4b8e1936c775574509c6204
Philip <philip_lorenz@icloud.com>
2021-10-14 10:24:18 +0200

    AHH

--- code/Application/cron60sec.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-$start = microtime(true);
-set_time_limit(60);
-for ($i = 0; $i < 59; ++$i) {
-    include("matching_algo.php");
-    time_sleep_until($start + $i + 1);
-}
--- code/Application/test.php
+++
@@ -1,8 +0,0 @@
-<?php
-// API URL
-$url = "https://quotes.rest/qod.json";
-
-// Create a new cURL resource
-$ch = curl_init($url);
-
-echo $ch;
