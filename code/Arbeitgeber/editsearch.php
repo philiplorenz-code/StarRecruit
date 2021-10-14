c70a66bc6e16955fa9c0f94b8a6352b654c81813
Philip <philip_lorenz@icloud.com>
2021-10-14 12:15:48 +0200

    df

--- code/Arbeitgeber/editsearch.php
+++ code/Arbeitgeber/editsearch.php
@@ -155,7 +155,7 @@
             <div class="fcf-form-group">
                 <label for="Message" class="fcf-label">Max. Gehalt</label>
                 <div class="fcf-input-group">
-                    <input type="text" class="fcf-form-control"" name="gehalt" value='<?php echo $entry['gehalt'];?>'>
+                    <input type="text" class="fcf-form-control" name="gehalt" value='<?php echo $entry['gehalt'];?>'>
 
                 </div>
             </div>
